e310759448a0d3cc613543e42a36fffbbab685ba
adekpoh9424 <42923599+adekpoh9424@users.noreply.github.com>
2019-12-31 13:45:45 -0600

    edit html and css, added the js file

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const alertBanner = document.getElementById('alert');
 
 alertBanner.innerHTML =
 
-<div class='alert-banner'>
+`<div class='alert-banner'>
     <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
     <p class='alert-banner-close'>X</p>
-</div>
+</div>`
 
 alertBanner.addEventListener('click', e => {
     const element = e.target;
@@ -72,4 +72,90 @@ let trafficChart = new CharacterData(trafficCanvas, {
     type: 'line',
     data: dayData,
     options: trafficOptions
+});
+
+// Bar Graph
+
+const dailyCanvas = document.getElementById('daily-chart');
+
+const dailyData = {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+
+const dailyOptions = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    },
+    legend: {
+        display: false
+    }
+}
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+
+
+//Mobile Doughnut Chart
+
+const mobileCanvas = document.getElementById('mobile-chart');
+
+const mobileData = {
+    labels: ['Desktop', 'Tablet', 'Phones'],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+const mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+        }
+    }
+};
+
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
+
+//User Search...
+
+const user = document.getElementById('userField');
+const message = document.getElementById('MessageField');
+const send = document.getElementById('sendField');
+
+send.addEventListener('click', () => {
+    if (user.value === '' && message.value === '') {
+        alert('Please fill out user and message fields before sending');
+    } else if (user.value === '') {
+        alert('Please fill out message field before sending');
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
+    }
 });
