07bb6d296af187066691ec024b2fe148d123ac6c
adekpoh9424 <42923599+adekpoh9424@users.noreply.github.com>
2020-01-06 14:48:15 -0600

    update html file

--- js/main.js
+++ js/main.js
@@ -14,6 +14,23 @@ alertBanner.addEventListener('click', e => {
     }
 });
 
+//Message
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+    if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+    alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+    alert("Please fill out message field before sending");
+    } else {
+    alert(`Message successfully sent to: ${user.value}`);
+    }
+});
 //Traffic Chart
 
 const trafficCanvas = document.getElementById('traffic-chart');
